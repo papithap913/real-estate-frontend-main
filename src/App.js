d953544786e283db95255d0f395382655eb9706c
papithap913 <papithap913@gmail.com>
2024-08-06 22:08:11 -0400

    Updated react-router-dom to use Routes instead of Switch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
-import PropertyManagement from './components/PropertyManagement';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/manage-properties" component={PropertyManagement} />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         {/* Add more routes as needed */}
-      </Switch>
+      </Routes>
     </Router>
   );
 }
